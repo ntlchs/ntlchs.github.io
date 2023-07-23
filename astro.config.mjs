b12daa38e44394c15f911bc32e565226630af126
Natalia Chies <nataliachies@gmail.com>
2023-07-23 20:28:52 -0300

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  site: "https://ntlchs.github.io",
+  base: "/src",
 });

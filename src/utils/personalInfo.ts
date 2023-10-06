f75d33246b6fc7108739bb17a5c4c6ad9d856860
Natalia Chies <nataliachies@gmail.com>
2023-10-06 06:24:32 -0300

    wip contacts

---
+++ src/utils/personalInfo.ts
@@ -0,0 +1,24 @@
+const contactInfo = {
+  name: "Natália Chies",
+  email: "nataliachies@gmail.com",
+  phone: "55 (51) 99732-3814",
+  location: "Porto Alegre, Brazil",
+  linkedin: "https://www.linkedin.com/in/nataliachies/",
+  github: "https://www.github.com/ntlchs",
+  website: "https://ntlchs.github.io/",
+};
+
+const techSkills = [
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Node.js",
+  "Sequelize",
+  "PostgreSQL",
+  "Git",
+  "HTML",
+  "CSS",
+  "Vercel",
+  "Google Cloud Platform",
+  "Figma",
+];

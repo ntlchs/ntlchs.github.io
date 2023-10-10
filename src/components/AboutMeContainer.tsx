6b0df82c72344ae3bbed2b79498e6a1b51f1bf8a
Natalia Chies <nataliachies@gmail.com>
2023-10-09 23:03:37 -0300

    finish color

--- src/components/AboutMeContainer.tsx
+++ src/components/AboutMeContainer.tsx
@@ -15,7 +15,7 @@ const AboutMeItem = ({
         {title}
       </a>
       <span className="text-gray"> since {started}</span>
-      <ul className="ml-4 list-disc marker:text-primary">
+      <ul className="ml-4 list-disc marker:animate-color-change">
         {activities.map((activity, index) => (
           <li key={index}>{activity}</li>
         ))}
@@ -28,7 +28,7 @@ const AboutMeContainer = () => {
   return (
     <div className="w-full my-6">
       <div className="flex flex-col items-center justify-center">
-        <div className="w-20 mx-auto h-20 ring-4 ring-primary rounded-full mb-4 overflow-hidden">
+        <div className="w-20 mx-auto h-20 border-4 animate-border-color-change rounded-full mb-4 overflow-hidden">
           <img src="/images/me.jpg" alt="Natalia" />
         </div>
         <p className="font-semibold">{contactInfo.name}</p>
